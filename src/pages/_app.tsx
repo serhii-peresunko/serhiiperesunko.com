cd9e9af857dbe3e814842542ecea2c7f9d049c04
serhii-peresunko <serhiiperesunko.main@gmail.com>
2025-03-31 22:09:43 +0300

    add GA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import "@/styles/globals.css";
 import "@/styles/common.css";
 import type { AppProps } from "next/app";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleTagManager gtmId="G-GPMMR2C5TG" />
+      </>
+   
+)}
+
+
+ 
+
