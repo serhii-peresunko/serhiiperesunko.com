ecac6dc0af7c30a14808e2e5b382c5d03a3b252e
serhii-peresunko <serhiiperesunko.main@gmail.com>
2025-03-31 13:33:31 +0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100vw',
+    fontSize: '16px',
+  }}>in progress...</div>
+}
